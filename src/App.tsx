fe42c319c3abc45ef1448d9e380a68d1f3cde45b
YUVAL SAHAR <yuvalsahar1466@gmail.com>
2021-11-10 23:29:02 +0200

    recipes list

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,126 @@ import * as ReactDOM from "react-dom";
 import { Routes, Route, Link } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
 import './App.css'
+import RecipesList from './recipesList/recipesList';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+const MOCK =[{
+  "id": 716429,
+    "title": "Pasta with Garlic, Scallions, Cauliflower & Breadcrumbs",
+    "image": "https://spoonacular.com/recipeImages/716429-556x370.jpg",
+    "imageType": "jpg",
+    "servings": 2,
+    "readyInMinutes": 45,
+    "license": "CC BY-SA 3.0",
+    "sourceName": "Full Belly Sisters",
+    "sourceUrl": "http://fullbellysisters.blogspot.com/2012/06/pasta-with-garlic-scallions-cauliflower.html",
+    "spoonacularSourceUrl": "https://spoonacular.com/pasta-with-garlic-scallions-cauliflower-breadcrumbs-716429",
+    "aggregateLikes": 209,
+    "healthScore": 19.0,
+    "spoonacularScore": 83.0,
+    "pricePerServing": 163.15,
+    "analyzedInstructions": [],
+    "cheap": false,
+    "creditsText": "Full Belly Sisters",
+    "cuisines": [],
+    "dairyFree": false,
+    "diets": [],
+    "gaps": "no",
+    "glutenFree": false,
+    "instructions": "",
+    "ketogenic": false,
+    "lowFodmap": false,
+    "occasions": [],
+    "sustainable": false,
+    "vegan": false
+},
+{"id": 716429,
+    "title": "Pasta with Garlic, Scallions, Cauliflower & Breadcrumbs",
+    "image": "https://spoonacular.com/recipeImages/716429-556x370.jpg",
+    "imageType": "jpg",
+    "servings": 2,
+    "readyInMinutes": 45,
+    "license": "CC BY-SA 3.0",
+    "sourceName": "Full Belly Sisters",
+    "sourceUrl": "http://fullbellysisters.blogspot.com/2012/06/pasta-with-garlic-scallions-cauliflower.html",
+    "spoonacularSourceUrl": "https://spoonacular.com/pasta-with-garlic-scallions-cauliflower-breadcrumbs-716429",
+    "aggregateLikes": 209,
+    "healthScore": 19.0,
+    "spoonacularScore": 83.0,
+    "pricePerServing": 163.15,
+    "analyzedInstructions": [],
+    "cheap": false,
+    "creditsText": "Full Belly Sisters",
+    "cuisines": [],
+    "dairyFree": false,
+    "diets": [],
+    "gaps": "no",
+    "glutenFree": false,
+    "instructions": "",
+    "ketogenic": false,
+    "lowFodmap": false,
+    "occasions": [],
+    "sustainable": false,
+    "vegan": false
+},{
+"id": 716429,
+    "title": "Pasta with Garlic, Scallions, Cauliflower & Breadcrumbs",
+    "image": "https://spoonacular.com/recipeImages/716429-556x370.jpg",
+    "imageType": "jpg",
+    "servings": 2,
+    "readyInMinutes": 45,
+    "license": "CC BY-SA 3.0",
+    "sourceName": "Full Belly Sisters",
+    "sourceUrl": "http://fullbellysisters.blogspot.com/2012/06/pasta-with-garlic-scallions-cauliflower.html",
+    "spoonacularSourceUrl": "https://spoonacular.com/pasta-with-garlic-scallions-cauliflower-breadcrumbs-716429",
+    "aggregateLikes": 209,
+    "healthScore": 19.0,
+    "spoonacularScore": 83.0,
+    "pricePerServing": 163.15,
+    "analyzedInstructions": [],
+    "cheap": false,
+    "creditsText": "Full Belly Sisters",
+    "cuisines": [],
+    "dairyFree": false,
+    "diets": [],
+    "gaps": "no",
+    "glutenFree": false,
+    "instructions": "",
+    "ketogenic": false,
+    "lowFodmap": false,
+    "occasions": [],
+    "sustainable": false,
+    "vegan": false
+},{
+"id": 716429,
+    "title": "Pasta with Garlic, Scallions, Cauliflower & Breadcrumbs",
+    "image": "https://spoonacular.com/recipeImages/716429-556x370.jpg",
+    "imageType": "jpg",
+    "servings": 2,
+    "readyInMinutes": 45,
+    "license": "CC BY-SA 3.0",
+    "sourceName": "Full Belly Sisters",
+    "sourceUrl": "http://fullbellysisters.blogspot.com/2012/06/pasta-with-garlic-scallions-cauliflower.html",
+    "spoonacularSourceUrl": "https://spoonacular.com/pasta-with-garlic-scallions-cauliflower-breadcrumbs-716429",
+    "aggregateLikes": 209,
+    "healthScore": 19.0,
+    "spoonacularScore": 83.0,
+    "pricePerServing": 163.15,
+    "analyzedInstructions": [],
+    "cheap": false,
+    "creditsText": "Full Belly Sisters",
+    "cuisines": [],
+    "dairyFree": false,
+    "diets": [],
+    "gaps": "no",
+    "glutenFree": false,
+    "instructions": "",
+    "ketogenic": false,
+    "lowFodmap": false,
+    "occasions": [],
+    "sustainable": false,
+    "vegan": false
+}]
   return (
     <>
       <BrowserRouter>
@@ -15,16 +131,15 @@ function App() {
           <div className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <div>What do you have?</div>
-            <input></input>
           </div>
         <Routes>
           <Route path="/" element={<h1>Home</h1>} />
           <Route path="/ingredients" element={<h1>Ingredients editor</h1>} />
-          <Route path="/recipes" element={<h1>Recipes List</h1>} />
+          <Route path="/recipes" element={<RecipesList recipes={MOCK}/>} />
           <Route path="/recipe-details" element={<h1>Recipe Details</h1>} />
         </Routes>
         </div>
-      </BrowserRouter>,
+      </BrowserRouter>
     </>
 
   )
